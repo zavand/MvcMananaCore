70d53854e0f8654a78110a8a683318cf8a060ea4
zavand <zavand@hotmail.com>
2020-02-22 12:38:56 -0500

    Translation to Russian for Sign/In and Sign/Up

--- BaseModel.cs
+++ BaseModel.cs
@@ -1,4 +1,7 @@
-﻿namespace Zavand.MvcMananaCore
+﻿using System.Globalization;
+using System.Threading;
+
+namespace Zavand.MvcMananaCore
 {
     public class BaseModel<TRoute, TController> : IBaseModel<TRoute>
         where TRoute : IBaseRoute
@@ -6,6 +9,7 @@
     {
         private TController Controller { get; set; }
         private TRoute Route { get; set; }
+        public CultureInfo SavedCurrentCultureBeforeAsyncAwait { get; set; }
 
         public virtual void SetupModel(TController controller, TRoute route)
         {
@@ -29,6 +33,17 @@
                 Controller != null &&
                 Controller.ModelState != null &&
                 Controller.ModelState.IsValid;
+        }
+
+        /// <summary>
+        /// Call this method inside view and layout to restore CultureInfo and make localization work.
+        /// async/await switches culture to default en-US
+        /// Some details here: 
+        /// https://stackoverflow.com/questions/30662668/keep-currentculture-in-async-await
+        /// </summary>
+        public void RestoreCultureAfterAsyncAwait()
+        {
+            Thread.CurrentThread.CurrentCulture = Thread.CurrentThread.CurrentUICulture = SavedCurrentCultureBeforeAsyncAwait;
         }
     }
 }
--- IBaseModel.cs
+++ IBaseModel.cs
@@ -1,8 +1,20 @@
-﻿namespace Zavand.MvcMananaCore
+﻿using System.Globalization;
+
+namespace Zavand.MvcMananaCore
 {
     public interface IBaseModel<out TRoute>
         where TRoute : IBaseRoute
     {
         TRoute GetRoute();
+        
+        /// <summary>
+        /// Call this method inside view and layout to restore CultureInfo and make localization work.
+        /// async/await switches culture to default en-US
+        /// Some details here: 
+        /// https://stackoverflow.com/questions/30662668/keep-currentculture-in-async-await
+        /// </summary>
+        void RestoreCultureAfterAsyncAwait();
+        
+        CultureInfo SavedCurrentCultureBeforeAsyncAwait { get; set; }
     }
 }
